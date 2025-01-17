a081873337d0164e78e64977f9417899e0292bc3
LeticiaLeon-Rodriguez <leonrodriguez.leticia@gmail.com>
2025-01-17 10:13:33 -0800

    query

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -35,12 +35,22 @@ const users = {
 
 app.use(express.json());
 
-app.get("/users", (req, res) => {
-  res.send(users);
-  console.log(
-    'working ok'
-  );
-});
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+      (user) => user["name"] === name
+    );
+  };
+  
+  app.get("/users", (req, res) => {
+    const name = req.query.name;
+    if (name != undefined) {
+      let result = findUserByName(name);
+      result = { users_list: result };
+      res.send(result);
+    } else {
+      res.send(users);
+    }
+  });
 
 app.listen(port, () => {
   console.log(
