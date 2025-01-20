f317ad25f63e8fb4a8df04fb77b14871b85e4267
LeticiaLeon-Rodriguez <leonrodriguez.leticia@gmail.com>
2025-01-19 23:30:15 -0800

    Delete method & find by name and job

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -74,6 +74,36 @@ app.post("/users", (req, res) => {
   res.send();
 });
 
+const deleteUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = deleteUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter((user) => user["name"] === name && user["job" === job]);
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  const job = req.query.job;
+
+  if (name && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
